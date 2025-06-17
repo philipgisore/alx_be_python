0206a379354af654f7933fe73a12823215e365c7
philipgisore <philipgisore7@gmail.com>
2025-06-17 22:52:18 +0300

    alx_be_python

--- main-0.py
+++ main-0.py
@@ -1,6 +1,7 @@
 import sys
 from bank_account import BankAccount
 
+
 def main():
     account = BankAccount(100)  # Example starting balance
     if len(sys.argv) < 2:
@@ -8,7 +9,7 @@ def main():
         print("Commands: deposit, withdraw, display")
         sys.exit(1)
 
-    command, *params = sys.argv[1].split(':')
+    command, *params = sys.argv[1].split(":")
     amount = float(params[0]) if params else None
 
     if command == "deposit" and amount is not None:
@@ -23,6 +24,7 @@ def main():
         account.display_balance()
     else:
         print("Invalid command.")
+
 
 if __name__ == "__main__":
     main()
