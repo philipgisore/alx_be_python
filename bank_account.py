28c259db4e40a19cb0a3ec3d42f0bd524bc5edbc
philipgisore <philipgisore7@gmail.com>
2025-06-17 23:48:59 +0300

    alx_be_python

--- bank_account.py
+++ bank_account.py
@@ -1,15 +1,15 @@
 class BankAccount:
-    def__init__(self,balance=0):
+    def __init__(self, balance=0):
         self.account_balance = balance
-        
-    def deposit(selft, amount);
+
+    def deposit(self, amount):
         self.account_balance += amount
-    
-    def withdraw(self,amount):
+
+    def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
             return True
         return False
-    
-    def diaplay_balance(self):
+
+    def display_balance(self):
         print(f"Current balance: ${self.account_balance:.2f}")
